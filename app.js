287d9fefaf8bd404192d9d9ba23334d3811b23ef
Fajriatun Munawaroh <fajriatun072@gmail.com>
2023-02-28 14:46:02 +0700

    First commit create tugas express.js

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+//menggunakan ejs
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact");
+});
+
+app.get("/category", (req, res) => {
+  res.render("category");
+});
+
+app.use("/", (req, res) => {
+  res.status(404);
+  res.send("<h1>404</h1>");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
